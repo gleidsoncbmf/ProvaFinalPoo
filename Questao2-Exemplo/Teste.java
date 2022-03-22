74b92be98eeda1ba6dd928f81193df59c585424b
gleidsoncbmf <89873935+gleidsoncbmf@users.noreply.github.com>
2022-03-21 21:19:13 -0300

    Add files via upload

---
+++ Questao2-Exemplo/Teste.java
@@ -0,0 +1,27 @@
+
+package Questao2;
+
+
+public class Teste {
+
+    
+    public static void main(String[] args) {
+        
+        double valorTotal = 1000.0;
+        double entrada = 100.0;
+        int parcelas = 10;
+        
+        
+        
+        try {
+            Financiamento f = new Financiamento(valorTotal, entrada, parcelas);
+            System.out.println(f.prestacao());
+            
+        }
+        catch(RuntimeException e ){
+            System.out.println(e.getMessage());
+        }
+        
+    }
+    
+}
